200e433a80bdd9cf0b9ff0ebfa78204d88c4a74a
unknown <bryansantos75@gmail.com>
2021-07-02 14:42:28 -0300

    criacao do repository e controller usuario

---
+++ app/src/main/java/com/vemdaterra/app/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.vemdaterra.app.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vemdaterra.app.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	public List<Usuario> findAllByEmailContainingIgnoreCase(String email);
+
+}
