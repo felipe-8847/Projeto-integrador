3650cb416c4f87ba3d44e1f74d56603174b5cb81
buchevitss <thais.buchweitz@outlook.com>
2021-08-06 15:27:35 -0300

    Task99

---
+++ app novo/src/main/java/com/vemdaterra/app/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.vemdaterra.app.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vemdaterra.app.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+		public List<Categoria>findAllByTipoContainingIgnoreCase(String tipo);
+}
