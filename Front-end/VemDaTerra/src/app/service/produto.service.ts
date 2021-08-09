4c74b65d9914e18b39d59fc67ed7f247fe3017e3
unknown <bryansantos75@gmail.com>
2021-08-09 18:16:55 -0300

    Implementação do put e delete com help do Lucao

--- Front-end/VemDaTerra/src/app/service/produto.service.ts
+++ Front-end/VemDaTerra/src/app/service/produto.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 import { Produto } from '../model/Produto';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,32 +13,27 @@ export class ProdutoService {
   constructor(private http: HttpClient) { }
 
   token = {
-     headers: new HttpHeaders().set('Authorization', environment.token)
-   }
-   getAllProduto(): Observable<Produto[]>{
-     return this.http.get<Produto[]>('https://vemdaterrabrasil.herokuapp.com/produto', this.token)
-   }
-   postProduto(produto: Produto): Observable<Produto>{
-     return this.http.post<Produto>('https://vemdaterrabrasil.herokuapp.com/produto', produto, this.token)
-   }
-  
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+  getAllProduto(): Observable<Produto[]> {
+    return this.http.get<Produto[]>('https://vemdaterrabrasil.herokuapp.com/produto', this.token)
+  }
   getByIdProduto(id: number): Observable<Produto> {
-     return this.http.get<Produto>(`https://vemdaterrabrasil.herokuapp.com/produto/${id}`, this.token)
+    return this.http.get<Produto>(`https://vemdaterrabrasil.herokuapp.com/produto/${id}`, this.token)
   }
-
-  getByNomeProduto(nome: string): Observable<Produto[]>{
-    return this.http.get<Produto[]>('https://vemdaterrabrasil.herokuapp.com/produto/nome', this.token)
+  getByDescricaoProduto(descricao: string): Observable<Produto[]> {
+    return this.http.get<Produto[]>(`https://vemdaterrabrasil.herokuapp.com/produto/descricao/${descricao}`, this.token)
   }
-
- 
-  putProduto(produto: Produto): Observable<Produto[]>{
-    return this.http.put<Produto[]>('https://vemdaterrabrasil.herokuapp.com/produto', this.token)
+  postProduto(produto: Produto): Observable<Produto> {
+    return this.http.post<Produto>('https://vemdaterrabrasil.herokuapp.com/produto', produto, this.token)
   }
+  putProduto(produto: Produto): Observable<Produto> {
+    return this.http.put<Produto>('https://vemdaterrabrasil.herokuapp.com/produto', this.token)
+  }
+  deleteProduto(id: number) {
+    return this.http.delete(`https://vemdaterrabrasil.herokuapp.com/produto/${id}`, this.token)
 
-  deleteProduto(produto: Produto){
-    return this.http.delete('https://vemdaterrabrasil.herokuapp.com/produto/${id}', this.token)
-    //(https:gabrielapaganini.herokuapp.com/postagens/${id}, this.token)
   }
 }
- 
- 
+
+
