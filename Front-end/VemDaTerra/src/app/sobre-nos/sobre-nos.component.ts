6980be87ca00c0af803aade409e62450d089d533
buchevitss <thais.buchweitz@outlook.com>
2021-08-11 16:06:24 -0300

    estilização do sobre nós

--- Front-end/VemDaTerra/src/app/sobre-nos/sobre-nos.component.ts
+++ Front-end/VemDaTerra/src/app/sobre-nos/sobre-nos.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sobre-nos',
-  templateUrl: './sobre-nos.component.html',
+  templateUrl:'./sobre-nos.component.html',
   styleUrls: ['./sobre-nos.component.css']
 })
 export class SobreNosComponent implements OnInit {
